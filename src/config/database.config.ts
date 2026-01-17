5cdf9ca3f9c506d4cf61c52214733fb96b471df7
Muath <muath.nahhas@yahoo.com>
2026-01-17 20:51:01 +0300

    seperate the databases connection in different file

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,23 +1,12 @@
 import { registerAs } from '@nestjs/config';
+import { mysqlConnection, pgConnection } from './database.connections';
 
 export default registerAs('database', () => {
   const env = process.env.NODE_ENV || 'development';
   const map = {
     development: {
-      mysql: {
-        host: process.env.DEV_MYSQL_HOST,
-        port: process.env.DEV_MYSQL_PORT,
-        username: process.env.DEV_MYSQL_USER,
-        password: process.env.DEV_MYSQL_PASSWORD,
-        database: process.env.DEV_MYSQL_DB,
-      },
-      postgres: {
-        host: process.env.DEV_PG_HOST,
-        port: process.env.DEV_PG_PORT,
-        username: process.env.DEV_PG_USER,
-        password: process.env.DEV_PG_PASSWORD,
-        database: process.env.DEV_PG_DB,
-      },
+      mysql: mysqlConnection(),
+      postgres: pgConnection(),
     },
   };
   return map[env];
