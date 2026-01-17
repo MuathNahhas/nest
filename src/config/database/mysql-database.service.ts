2e1ad971599a05ec3cf55935f47eb04cac2653c1
Muath <muath.nahhas@yahoo.com>
2026-01-17 21:33:55 +0300

    revamp the connections with databases

--- src/config/database/mysql-database.service.ts
+++ src/config/database/mysql-database.service.ts
@@ -1,20 +1,28 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as mysql from 'mysql2/promise';
 @Injectable()
-export class MysqlDb implements OnModuleInit {
-  private connection: mysql.Connection;
+export class MysqlDb implements OnModuleInit, OnModuleDestroy {
+  private pool: mysql.Pool;
   constructor(private configService: ConfigService) {}
 
   async onModuleInit() {
     const config = this.configService.get('database.mysql');
-    this.connection = await mysql.createConnection({
+    this.pool = await mysql.createPool({
       host: config.host,
       port: config.port,
       user: config.username,
       password: config.password,
       database: config.database,
+
+      waitForConnections: true,
+      connectionLimit: 20,
+      queueLimit: 0,
     });
     console.log('✅ MySQL connected:', config.host);
+  }
+  async onModuleDestroy() {
+    await this.pool.end();
+    console.log('MySQL Pool closed');
   }
 }
