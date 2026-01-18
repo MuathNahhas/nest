37f70e813e2883f08c03db70d983934187ab4fd3
Muath <muath.nahhas@yahoo.com>
2026-01-18 19:17:08 +0300

    finish the kysely-config

--- src/config/database/mysql-database.service.ts
+++ src/config/database/mysql-database.service.ts
@@ -1,28 +1,40 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as mysql from 'mysql2/promise';
+import { Kysely, MysqlDialect } from 'kysely';
+import { createPool, Pool } from 'mysql2';
+import { MysqlDatabase } from './mysql/mysql.database';
+
 @Injectable()
 export class MysqlDb implements OnModuleInit, OnModuleDestroy {
-  private pool: mysql.Pool;
-  constructor(private configService: ConfigService) {}
+  public MysqlDatabase!: Kysely<MysqlDatabase>;
+  private pool!: Pool;
+
+  constructor(private readonly configService: ConfigService) {}
 
   async onModuleInit() {
     const config = this.configService.get('database.mysql');
-    this.pool = await mysql.createPool({
+    this.pool = createPool({
       host: config.host,
       port: config.port,
       user: config.username,
       password: config.password,
       database: config.database,
-
       waitForConnections: true,
       connectionLimit: 20,
       queueLimit: 0,
     });
+
+    this.MysqlDatabase = new Kysely<MysqlDatabase>({
+      dialect: new MysqlDialect({ pool: this.pool }),
+    });
+
     console.log('✅ MySQL connected:', config.host);
   }
+
   async onModuleDestroy() {
-    await this.pool.end();
-    console.log('MySQL Pool closed');
+    if (this.pool) {
+      await this.pool.end();
+      console.log('MySQL pool closed');
+    }
   }
 }
--- src/config/database/postgress-database.service.ts
+++ src/config/database/postgress-database.service.ts
@@ -1,14 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Pool } from 'pg';
+import { Kysely, PostgresDialect } from 'kysely';
+import { PgDatabase } from './postgres/pg.database';
+
 @Injectable()
 export class PgDb implements OnModuleInit {
   private pool: Pool;
+  public PgDatabase!: Kysely<PgDatabase>;
   constructor(private configService: ConfigService) {}
 
   async onModuleInit() {
     const config = await this.configService.get('database.postgres');
-    this.pool = await new Pool({
+    ((this.pool = await new Pool({
       host: config.host,
       port: config.port,
       user: config.username,
@@ -17,7 +21,10 @@ export class PgDb implements OnModuleInit {
       max: 20,
       idleTimeoutMillis: 30000,
       connectionTimeoutMillis: 3000,
-    });
+    })),
+      (this.PgDatabase = new Kysely<PgDatabase>({
+        dialect: new PostgresDialect({ pool: this.pool }),
+      })));
     console.log('✅ PostgreSQL connected:', config.host);
   }
   async onModuleDestroy() {
