af5ec63996bb74a2a5745dfe9ad956338f860c91
Muath <muath.nahhas@yahoo.com>
2026-01-26 16:07:32 +0300

    implement retry connect to database with not connect on starting app or on docker or cloud

--- src/config/database/mysql/mysql-database.service.ts
+++ src/config/database/mysql/mysql-database.service.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { Kysely, MysqlDialect } from 'kysely';
 import { createPool, Pool } from 'mysql2';
 import { MysqlDatabase } from './mysql.database';
-
+import pRetry from 'p-retry';
 @Injectable()
 export class MysqlDb implements OnModuleInit, OnModuleDestroy {
   public MysqlDatabase: Kysely<MysqlDatabase>;
@@ -13,24 +13,34 @@ export class MysqlDb implements OnModuleInit, OnModuleDestroy {
 
   async onModuleInit() {
     const config = this.configService.get('database.mysql');
-    this.pool = createPool({
-      host: config.host,
-      port: config.port,
-      user: config.username,
-      password: config.password,
-      database: config.database,
-      waitForConnections: true,
-      connectionLimit: 20,
-      queueLimit: 0,
-    });
+    await pRetry(
+      async () => {
+        this.pool = createPool({
+          host: config.host,
+          port: config.port,
+          user: config.username,
+          password: config.password,
+          database: config.database,
+          waitForConnections: true,
+          connectionLimit: 20,
+          queueLimit: 0,
+        });
 
-    this.MysqlDatabase = new Kysely<MysqlDatabase>({
-      dialect: new MysqlDialect({ pool: this.pool }),
-    });
-
-    console.log('✅ MySQL connected:', config.host);
+        this.MysqlDatabase = new Kysely<MysqlDatabase>({
+          dialect: new MysqlDialect({ pool: this.pool }),
+        });
+        console.log('✅ MySQL connected:', config.host);
+      },
+      {
+        retries: 5,
+        onFailedAttempt: (error) => {
+          console.warn(
+            `❌ MySQL connection failed (attempt ${error.attemptNumber}): ${error.error?.message || error.error}}`,
+          );
+        },
+      },
+    );
   }
-
   async onModuleDestroy() {
     if (this.pool) {
       await this.pool.end();
